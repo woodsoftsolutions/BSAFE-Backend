88008fbf401aaac3c8843b9c2683d8208a66743c
Bladimir Aponte <bladimirapon@gmail.com>
2025-06-10 18:20:26 -0400

    cores allowed

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000', 'http://5.182.17.212', 'http://5.182.17.212:8080'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
